dd08c7a617e825ac9b7fb6318b855f03f88e473e
Cameron <camrk13@gmail.com>
2017-11-30 16:23:57 -0500

    checkboard style

--- Checkerboard.js
+++ Checkerboard.js
@@ -9,18 +9,18 @@ function onLoadBody () {
 			/*Even Rows*/
 			if(i%2==0){ 
 				if(j%2 == 0){ //even columns
-					table.rows[i].cells[j].innerHTML = "red";
+					table.rows[i].cells[j].style.backgroundColor = "red";
 				}else{
-					table.rows[i].cells[j].innerHTML = "black";	
+					table.rows[i].cells[j].style.backgroundColor = "black";	
 				}
 			}
 
 			/*Odd Rows*/
 			else{
 				if(j%2 != 0){
-					table.rows[i].cells[j].innerHTML = "red";
+					table.rows[i].cells[j].style.backgroundColor = "red";
 				}else{
-					table.rows[i].cells[j].innerHTML = "black";	
+					table.rows[i].cells[j].style.backgroundColor = "black";	
 				}
 			}
 			
